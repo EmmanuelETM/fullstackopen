66c5ccf9cb299897d9de9e47eaa19ed245575293
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-06-25 22:52:02 -0400

    changed stuff

--- part1/ex_2/src/App.jsx
+++ part1/ex_2/src/App.jsx
@@ -1,29 +1,35 @@
-import { useState} from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      {counter}
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
-
-  // return (
-  //   <div>
-  //     <button onClick={() => setCounter((counter) => counter + 1)}>
-  //       {counter}
-  //     </button>
-  //   </div>
-  // )
-  
 }
-
 
 export default App
