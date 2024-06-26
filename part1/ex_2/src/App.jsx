9004f8d211b978b49403f4ee25296f2106f984ad
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-06-25 23:16:37 -0400

    Epico

--- part1/ex_2/src/App.jsx
+++ part1/ex_2/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>{name} you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const Test = (props) => {
+  const {foo, state} = props
+  console.log(foo, state)
+  return <></>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name = 'PooPoo'
+  const age = 35
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hola Mundo</h1>
+        <Hello name="PeePee" age={20}/>
+        <Hello name={name} age={age}/>
+        <Test foo={1} bar="FooBar cool" state={6}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
