aee2b8dc1fb184eff025c6105b7e8e8457436795
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-07-09 20:51:29 -0400

    added ex_clases

---
+++ ex_clases/test/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Block from './Block';
+
+const App = () => (
+  <div>
+    <h1>Ejercicio</h1>
+    <Block />
+  </div>
+);
+
+export default App
